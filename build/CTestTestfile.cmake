7404f214ad8e7e18644f35b6acf9a17a540d8faa
Tom Malone <tmalone+git@gmail.com>
2015-12-12 03:51:58 +0000

    Initial project import [github]

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/rock/Projects/ROS/programming_ros/src
+# Build directory: /home/rock/Projects/ROS/programming_ros/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/rock/Projects/ROS/programming_ros/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/rock/Projects/ROS/programming_ros/devel;/opt/ros/indigo".split(';') if "/home/rock/Projects/ROS/programming_ros/devel;/opt/ros/indigo" != "" else []
