52e1f001f51377adbb2f97e0ce3d8ce70a7f1439
Tom Malone <tmalone+git@gmail.com>
2015-12-12 03:55:41 +0000

    Added initialization script for ROS, which spawns the requisite environment variables and sets the correct distro (currently Indigo).

---
+++ init_ros.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+
+PROJECT_NAME='Programming ROS'
+PROJECT_DIR="${HOME}/Projects/ROS/programming_ros"
+
+# Initialize ROS
+source /opt/ros/${ROS_DISTRO}/setup.zsh
+source ~/.zshrc
+source "${PROJECT_DIR}/devel/setup.zsh"
+
+
