a766aa2bdfe59f84f7607bdadca9bb57582a886f
Tom Malone <tmalone+git@gmail.com>
2015-12-12 03:57:27 +0000

    Added feedback output to initialization script.

--- init_ros.sh
+++ init_ros.sh
@@ -4,6 +4,7 @@ PROJECT_NAME='Programming ROS'
 PROJECT_DIR="${HOME}/Projects/ROS/programming_ros"
 
 # Initialize ROS
+echo "Initializing ROS environment variables..."
 source /opt/ros/${ROS_DISTRO}/setup.zsh
 source ~/.zshrc
 source "${PROJECT_DIR}/devel/setup.zsh"
